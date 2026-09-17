2716dae8d6aad4ae504f3e0bdcd96f307681d352
Rafael <rafaelalmeidanh@gmail.com>
2026-09-17 19:56:41 -0300

    Commit nova branch

---
+++ Grupo3AULA1Rafael.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula1cursojava;
+
+/**
+ *
+ * @author rafae_x4pciw0
+ */
+public class Grupo3AULA1Rafael {
+
+    public static void main(String[] args) {  //COMEÇO DA MAIN
+    
+    int pontos = 80;
+
+    boolean portaoaberto = false;
+
+if (pontos < 50 || portaoaberto == false)
+        {
+            System.out.println("Voce Conseguiu!");
+}
+    
+        else if (pontos >= 50 && pontos <= 70 && portaoaberto == true)
+        {
+            System.out.println("Voce Escapou!");
+    }
+        else if (pontos > 70 && portaoaberto == true)
+        {
+            System.out.println("FASE BONUS DESBLOQUEADA!");
+}    
+
+
+
+}         //FIM DA MAIN
+    
+
+
+}
