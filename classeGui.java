a310176df259fae4325b9ccfdd80363f0128a880
Vinicius <viniciusmolinapereira23@gmail.com>
2026-09-17 19:57:00 -0300

    Commit nova branch

---
+++ classeGui.java
@@ -0,0 +1,12 @@
+public class classeGui {
+    public static void main(String[] args){
+        System.out.println("Teste do Git");
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(".");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
